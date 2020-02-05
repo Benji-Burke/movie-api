af4250c12bb0f09d0f75bdbb31c99b3a70d69f43
Ben <benjaminstevenburke@gmail.com>
2020-02-05 10:39:34 -0500

    adding the new extensions

--- src/main/java/com/revature/servlets/TestServlet.java
+++ src/main/java/com/revature/servlets/TestServlet.java
@@ -12,7 +12,7 @@ public class TestServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write("Application successfully deployed using AWS CodeBuild, CodePipeline, and Elastic Beanstalk");
+        resp.getWriter().write("Application successfully deployed using AWS CodeBuild, CodePipeline, and Elastic Beanstalk, please work! ");
     }
 
 }
